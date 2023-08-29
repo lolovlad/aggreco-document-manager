2c1d8349a521edfbf6029b4d70d5a78b891999d0
lolovlad <lolovlad33@gmail.com>
2023-08-29 14:12:36 +0400

    start_commit

--- main.py
+++ main.py
@@ -16,28 +16,28 @@ from Classes.FileParser.DocxFile import DocxFile
 
 from Classes.FileParser.XlsxFile import XlsxFile
 
-path_file_json = Path("Files", "Docx", "Form1.json")
+"""path_file_json = Path("Files", "Docx", "Form1.json")
 path_file_docx = Path("Files", "Docx", "Form1.docx")
-path_file_xlsx = Path("Files", "Docx", "test.xlsx")
+path_file_xlsx = Path("Files", "Docx", "test.xlsx")"""
 
-'''
+"""
 parser = ParserTemplateFile(path_file_docx)
 parser.parser()
 
 json_file = JsonFile(path_file_json, parser.file_schema)
-json_file.save_file()'''
+json_file.save_file()"""
 
-
-'''json_file = JsonFile(path_file_json)
+"""
+json_file = JsonFile(path_file_json)
 json_file.read_file()
 
 builder_xlsx = BuilderXlsxFile(path_file_xlsx, FileSchemas.model_validate(json_file.scheme))
 
 builder_xlsx.build()
-print(builder_xlsx.json_data_schema)'''
+print(builder_xlsx.json_data_schema)"""
 
-'''
-json_map_data_path = Path("Files", "Docx", "test.json")
+
+"""json_map_data_path = Path("Files", "Docx", "test.json")
 json_map_data_file = JsonFile(json_map_data_path)
 json_map_data_file.read_file()
 
@@ -45,7 +45,7 @@ parser_exel = ParserFormFile(path_file_xlsx, FileSchemas(), json_map_data_file.s
 parser_exel.parser()
 
 builder_docx = BuilderDocxFile(Path("Files", "Docx", "text_template.docx"), path_file_docx, parser_exel.map_data)
-builder_docx.build()
-'''
+builder_docx.build()"""
+
 
 
